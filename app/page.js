99bf73d8e5969e365e8b6884b799b3713bea12cc
bgilross <bgilross@gmail.com>
2024-12-17 18:53:07 -0800

    Co-authored-by: rawdawgmcgraw <rawdawgmcgraw@users.noreply.github.com>

--- app/page.js
+++ app/page.js
@@ -1,14 +1,15 @@
+import Image from "next/image"
+
 export default function Home() {
-  return (
-    <div className='h-screen w-full flex flex-col justify-center items-center'>
-      <div className='h-12 w-1/2 mb-14 rounded-3xl bg-black opacity-60'></div>
-      <img
-        className='animate-fadeIn mb-10'
-        src='/logoBG.png'
-        alt='logo'
-        width={900}
-        height={700}
-      />
-    </div>
-  );
+	return (
+		<div className="h-screen w-full flex flex-col justify-center items-center">
+			<Image
+				className="animate-fadeIn mb-12"
+				src="/logoBG.png"
+				alt="logo"
+				width={900}
+				height={700}
+			/>
+		</div>
+	)
 }
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,29 +1,30 @@
+/* eslint-disable import/no-anonymous-default-export */
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-      backgroundImage: {
-        main: "url(/public/mainBG.png)",
-      },
-      keyframes: {
-        fadeIn: {
-          "0%": { opacity: "0" },
-          "100%": { opacity: "1" },
-        },
-      },
-      animation: {
-        fadeIn: "fadeIn 2s ease-in",
-      },
-    },
-  },
-  plugins: [],
-};
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+			},
+			backgroundImage: {
+				main: "url(/public/mainBG.png)",
+			},
+			keyframes: {
+				fadeIn: {
+					"0%": { opacity: "0" },
+					"100%": { opacity: "1" },
+				},
+			},
+			animation: {
+				fadeIn: "fadeIn 2s ease-in",
+			},
+		},
+	},
+	plugins: [],
+}
