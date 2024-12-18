6dc857907b2a2b3102f4b1177a8bf30cb41db791
HippieJim <hippiejim2896@gmail.com>
2024-12-17 20:09:09 -0600

    Start top nav bar
    Co-authored-by: bgilross <bgilross@users.noreply.github.com>

--- app/page.js
+++ app/page.js
@@ -1,8 +1,9 @@
 export default function Home() {
   return (
-    <div className='h-screen w-full flex justify-center items-center'>
+    <div className='h-screen w-full flex flex-col justify-center items-center'>
+      <div className='h-12 w-1/2 mb-14 rounded-3xl bg-black opacity-60'></div>
       <img
-        className='animate-fadeIn'
+        className='animate-fadeIn mb-10'
         src='/logoBG.png'
         alt='logo'
         width={900}
